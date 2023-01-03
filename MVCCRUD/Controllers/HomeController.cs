937e25103ac38d2d1a8a96f5c20c458db0d69ed8
viveksankrit <vivek.sankrit@nagarro.com>
2023-01-03 23:07:57 +0530

    Assignment

---
+++ MVCCRUD/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCCRUDDBContext _context = new MVCCRUDDBContext();
+        public ActionResult Index()
+        {
+            var listOfData = _context.Students.ToList();
+            return View(listOfData);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student model)
+        {
+            _context.Students.Add(model);
+            _context.SaveChanges();
+            ViewBag.Message = "Data inserted Successfully";
+            return View();
+        }
+       
+    }
+}
